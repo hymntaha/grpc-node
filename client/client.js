1ff4440e29d611c4f582d0389b7c69d47ab6851f
hymntaha <tahauygun@gmail.com>
2020-08-30 00:28:20 -0700

    client update

--- client/client.js
+++ client/client.js
@@ -8,6 +8,25 @@ const services = require('../../protos/dummy_grpc_pb');
 function main() {
   console.log('hello from client');
   const client = new service.GreetServiceClient('localhost:50051', grpc.credentials.createInsecure())
+
+
+  const request = new greets.GreetRequest();
+
+  const greeting = new greets.Greeting();
+
+  greeting.setFirstName("Jerry");
+  greeting.setLastName("Tom");
+
+  request.setGreeting(greeting);
+  client.greet(request, (error, response) => {
+    if (!error) {
+      console.log("Greeting response: ", response.getResult());
+    } else {
+      console.log(error);
+    }
+  })
 }
+
+
 
 main()
