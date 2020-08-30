83baa4e5f515c8ac34a1f5f28b5e65a4f08ac678
hymntaha <tahauygun@gmail.com>
2020-08-30 00:31:51 -0700

    client bug fix

--- client/client.js
+++ client/client.js
@@ -3,8 +3,6 @@ const service = require('../server/protos/greet_grpc_pb');
 
 const grpc = require('grpc');
 
-const services = require('../../protos/dummy_grpc_pb');
-
 function main() {
   console.log('hello from client');
   const client = new service.GreetServiceClient('localhost:50051', grpc.credentials.createInsecure())
