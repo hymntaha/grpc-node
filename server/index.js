b0b6a96318abb44544d618b63c4969e035ba0e52
hymntaha <tahauygun@gmail.com>
2020-09-02 23:05:17 -0700

    adding simple calculator in grpc

--- server/index.js
+++ server/index.js
@@ -2,6 +2,16 @@ const grpc = require('grpc');
 const greets = require('../server/protos/greet_pb');
 const service = require('../server/protos/greet_grpc_pb');
 
+const calc = require('../server/protos/calculator_pb');
+const calcService = require('../server/protos/calculator_grpc_pb');
+
+function sum(call, callback) {
+  var sumResponse = new calc.SumResponse()
+
+  sumResponse.setSumResult(call.request.getFirstNumber() + call.request.getSecondNumber())
+  callback(null, sumResponse)
+}
+
 
 /*
   Implements the greet RPC method.
@@ -16,10 +26,15 @@ function greet(call, callback) {
   callback(null, greeting)
 
 }
+
 function main() {
   var server = new grpc.Server();
-  server.addService(service.GreetServiceService, {
-    greet: greet
+  // server.addService(service.GreetServiceService, {
+  //   greet: greet
+  // });
+
+  server.addService(calcService.CalculatorServiceService, {
+    sum: sum
   });
 
   server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure())
