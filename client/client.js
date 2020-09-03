ac8fa2e6898dbc6e307d91ed8d9cd80caae4a06b
hymntaha <tahauygun@gmail.com>
2020-09-02 23:09:22 -0700

    calculator client side

--- client/client.js
+++ client/client.js
@@ -1,10 +1,28 @@
 const greets = require('../server/protos/greet_pb');
 const service = require('../server/protos/greet_grpc_pb');
 
+const calc = require('../server/protos/calculator_pb');
+const calcService = require('../server/protos/calculator_grpc_pb');
+
 const grpc = require('grpc');
 
-function main() {
-  console.log('hello from client');
+function callSum(){
+  const client = new calcService.CalculatorServiceClient('localhost:50051', grpc.credentials.createInsecure())
+  var sumRequest = new calc.SumRequest()
+
+  sumRequest.setFirstNumber(10)
+  sumRequest.setSecondNumber(15)
+
+  client.sum(sumRequest, (error, response)=>{
+    if (!error) {
+      console.log(sumRequest.getFirstNumber() + " + " + sumRequest.getSecondNumber() + " = " + response.getSumResult());
+    } else {
+      console.log(error);
+    }
+  })
+}
+
+function callGreetings(){
   const client = new service.GreetServiceClient('localhost:50051', grpc.credentials.createInsecure())
 
 
@@ -23,6 +41,11 @@ function main() {
       console.log(error);
     }
   })
+}
+
+
+function main() {
+  callGreetings()
 }
 
 
